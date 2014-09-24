e71753861c4bef8d8448e7dd3920141863390c8b
Luiz Cezer <lccezinha@gmail.com>
2014-09-24 00:09:38 -0300

    Improve seeds :dancers:

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-author_one = Author.create! name: 'Luiz Cezer'
-author_two = Author.create! name: 'Cezer Luiz'
+author_one   = Author.create! name: 'Luiz Cezer'
+author_two   = Author.create! name: 'Dave Mustaine'
+author_three = Author.create! name: 'James Hetfield'
 
 category_one   = Category.create! name: 'Esportes'
 category_two   = Category.create! name: 'Programação'
 category_three = Category.create! name: 'Cultura'
+
+words = %w(Metallica band Megadeth awesome concert the big four
+           Dave Miojão James YEAH crowd Brazil Rock in Rio better
+           rust in peace master of puppets gzuis Slayer Array Reign in Blood
+           Creeping Master release new slipknto album Corey Taylor Metallica
+           Massacatrion Anthrax Belladona Joey Jordison the new concert
+           Exodus album Steve Zetro return to band now and Ghost bc is cool)
+
+500.times do |index|
+  puts "Creating: #{index + 1}"
+  title_size, description_size = index.odd? ? [3, 20] : [2, 35]
+
+  Post.create! title: words.shuffle.first(title_size).join(' ').titleize,
+               description: words.shuffle.first(description_size).join(' ').camelize,
+               author_id: Author.order('RANDOM()').first.id,
+               category_id: Category.order('RANDOM()').first.id
+
+end
