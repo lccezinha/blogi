b5436909803f514962efd756f67aa55e7c69ba97
Luiz Cezer <lccezinha@gmail.com>
2014-09-30 14:08:40 -0300

    Basic indexation :dancers:

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ActiveRecord::Base
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
+  index_name 'blogi'
+  document_type 'post'
+
   belongs_to :author
   belongs_to :category
 
