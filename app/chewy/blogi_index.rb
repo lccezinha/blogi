63f6b0e5d86a739e696dba1645d7290251c4a9a8
Luiz Cezer <lccezinha@gmail.com>
2014-10-31 10:27:57 -0200

    Chewy config + base index

--- app/chewy/blogi_index.rb
+++ app/chewy/blogi_index.rb
@@ -1,2 +1,17 @@
 class BlogiIndex < Chewy::Index
+  settings analysis: {
+    analyzer: {
+      title: {
+        tokenizer: 'standard',
+        filter: ['lowercase', 'asciifolding']
+      }
+    }
+  }
+
+  define_type Post.includes(:author, :category), name: 'post' do
+    field :title, analyzer: 'title'
+    field :description
+    field :author, value: ->{ author.name }
+    field :category, value: ->{ category.name }
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,6 @@ class Post < ActiveRecord::Base
 
   validates :title, presence: true
   validates :description, presence: true
+
+  update_index('blogi#post') { self }
 end
